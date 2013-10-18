acca9d525d79af69f40fc38e9d1859d9c345158f
Sander <Sander@Sander-PC>
2013-10-18 08:38:29 +0300

    Commiting a working apache logger, all linked up

--- POS/src/ee/ut/math/tvt/ateamplusone/Intro.java
+++ POS/src/ee/ut/math/tvt/ateamplusone/Intro.java
@@ -1,11 +1,18 @@
 package ee.ut.math.tvt.ateamplusone;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import org.apache.log4j.Logger;
+
+/**
+* Log4j logger
+*/
 
 public class Intro extends JFrame{
 
 	private static final long serialVersionUID = 1L;
+	static Logger log4j = Logger.getLogger("test.program");
 
 	public Intro(){
 		
@@ -20,6 +27,7 @@ public class Intro extends JFrame{
 	}
 
 	public static void main(String[] args) {
+		log4j.debug("Creating screen");
 		new Intro();
 	}
 }
