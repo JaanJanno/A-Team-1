a77625c3f35e3276147e46caea3eef7aea9b5d7a
TKasekamp <toniskasekamp@gmail.com>
2013-11-28 13:49:51 +0200

    Test classes from Sander

--- POS/test/ee/ut/math/tvt/HistoryItemTest.java
+++ POS/test/ee/ut/math/tvt/HistoryItemTest.java
@@ -1,24 +1,63 @@
 package ee.ut.math.tvt;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.HistoryItem;
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
 public class HistoryItemTest {
-	
-	public void testAddSoldItem(){
-		
+	private ArrayList<SoldItem> goods;
+	private StockItem temp;
+	private StockItem temp2;
+	private HistoryItem test;
+
+	@Before
+	public void setUp() {
+		temp = new StockItem((long) 5, "Test", "Test", 15);
+		temp2 = new StockItem((long) 6, "Test2", "Test2", 15);
+
 	}
-	
-    public void testGetSumWithNoItems(){
-    	
-    }
-    
-    public void testGetSumWithOneItem(){
-    	
-    }
-    
-    public void testGetSumWithMultipleItems() {
-    	
-    }
+
+	@Test
+	public void testAddSoldItem() {
+		goods = new ArrayList<SoldItem>();
+		goods.add(new SoldItem(temp, 10));
+		goods.add(new SoldItem(temp2, 10));
+		test = new HistoryItem(goods);
+		ArrayList<SoldItem> goods2 = new ArrayList<SoldItem>();
+		goods2 = (ArrayList<SoldItem>) test.getGoods();
+
+		assertTrue(goods.equals(goods2));
+	}
+
+	@Test
+	public void testGetSumWithNoItems() {
+		goods = new ArrayList<SoldItem>();
+		test = new HistoryItem(goods);
+		assertTrue(test.getSum() == 0);
+	}
+
+	@Test
+	public void testGetSumWithOneItem() {
+		goods = new ArrayList<SoldItem>();
+		goods.add(new SoldItem(temp, 10));
+		test = new HistoryItem(goods);
+
+		assertTrue(test.getSum() == 150);
+	}
+
+	@Test
+	public void testGetSumWithMultipleItems() {
+		goods = new ArrayList<SoldItem>();
+		goods.add(new SoldItem(temp, 10));
+		goods.add(new SoldItem(temp2, 10));
+		test = new HistoryItem(goods);
+		assertTrue(test.getSum() == 300);
+	}
 }
--- POS/test/ee/ut/math/tvt/StockTableModelTest.java
+++ POS/test/ee/ut/math/tvt/StockTableModelTest.java
@@ -1,24 +1,55 @@
 package ee.ut.math.tvt;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.StockTableModel;
+
 public class StockTableModelTest {
-	
-	public void testValidateNameUniqueness(){
-		
+	private StockTableModel test;
+	private StockItem temp;
+
+	@Before
+	public void setUp() {
+		test = new StockTableModel();
+		temp = new StockItem((long) 5, "Test", "Test", 15);
+
 	}
-	
-	public void testHasEnoughInStock(){
-		
+
+	@Test
+	public void testValidateNameUniqueness() {
+		test.clear();
+
+		test.addItem(temp);
+		List<StockItem> control = test.getTableRows();
+		test.addItem(temp);
+		assertTrue(control.equals(test.getTableRows()));
 	}
-	
-	public void testGetItemByIdWhenItemExists(){
-		
+
+	public void testHasEnoughInStock() {
+
 	}
-	
+
+	@Test
+	public void testGetItemByIdWhenItemExists() {
+		test.clear();
+		test.addItem(temp);
+		StockItem result = test.getItemById((long) 5);
+		assertTrue(temp.equals(result));
+	}
+
+	@Test(expected = NoSuchElementException.class)
 	public void testGetItemByIdWhenThrowsException() {
-		
+		test.clear();
+		StockItem result = null;
+		result = test.getItemById((long) 5);
+		assertTrue(result == null);
 	}
 }
