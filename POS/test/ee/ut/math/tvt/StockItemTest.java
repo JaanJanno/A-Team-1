8e7ee379490c2aed8fec8fde448d2e058b303b89
Tõnis Kasekamp <toniskasekamp@gmail.com>
2013-11-28 23:16:25 +0200

    testClone() fix

--- POS/test/ee/ut/math/tvt/StockItemTest.java
+++ POS/test/ee/ut/math/tvt/StockItemTest.java
@@ -9,17 +9,26 @@ import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class StockItemTest {
 
+	private StockItem test;
+	
+	@Before
+	public void setUp() {
+		test = new StockItem((long) 5, "Test", "Test", 15,10);
+	}
 	@Test
 	public void testClone() {
-		StockItem test = new StockItem((long) 5, "Test", "Test", 15);
-		Object test2 = test.clone();
-		StockItem test3 = (StockItem) test2;
-		assertTrue(test.equals(test3));
+		StockItem test3 = (StockItem) test.clone();
+		// Checking fields individually as test.equals(test3) will return false
+		boolean flag1 = (test.getId() == test3.getId());
+		boolean flag2 = test.getName().equals(test3.getName());
+		boolean flag3 = test.getDescription().equals(test3.getDescription());
+		boolean flag4 = test.getPrice() == test3.getPrice();
+		boolean flag5 = test.getQuantity() == test3.getQuantity();
+		assertTrue(flag1 && flag2 && flag3 && flag4 && flag5);
 	}
 
 	@Test
 	public void testGetColumn() {
-		StockItem test = new StockItem((long) 5, "Test", "Test", 15, 10);
 		boolean flag1 = ((long) (test.getColumn(0))) == 5;
 		boolean flag2 = ((String) (test.getColumn(1))).equalsIgnoreCase("Test");
 		boolean flag3 = ((Double) (test.getColumn(2))) == 15;
