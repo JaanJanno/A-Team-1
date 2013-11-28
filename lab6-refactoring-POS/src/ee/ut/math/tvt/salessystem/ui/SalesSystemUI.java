31ddc4e902f96742e4dc6ed21ebda93df897dce3
Juhan-Rasmus Risti <juhan@risti.eu>
2013-11-28 22:46:21 +0200

    Another cleanup.

--- lab6-refactoring-POS/src/ee/ut/math/tvt/salessystem/ui/SalesSystemUI.java
+++ lab6-refactoring-POS/src/ee/ut/math/tvt/salessystem/ui/SalesSystemUI.java
@@ -21,75 +21,57 @@ import org.apache.log4j.Logger;
  * Graphical user interface of the sales system.
  */
 public class SalesSystemUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger(SalesSystemUI.class);
+	private SalesSystemModel model;
+	private PurchaseTab purchaseTab;
+	private HistoryTab historyTab;
+	private StockTab stockTab;
+	private ClientTab clientTab;
+	private SalesDomainController domainController;
 
-  private static final long serialVersionUID = 1L;
+	/**
+	 * Constructs sales system GUI.
+	 * 
+	 * @param domainController
+	 *            Sales domain controller.
+	 */
+	public SalesSystemUI(SalesDomainController domainController) {
+		this.domainController = domainController;
+		this.model = new SalesSystemModel(domainController);
+		domainController.setModel(model);
+		historyTab = new HistoryTab(model);
+		stockTab = new StockTab(model, domainController);
+		purchaseTab = new PurchaseTab(domainController, model, this);
+		clientTab = new ClientTab(model);
+		setTitle("Sales system");
+		try {
+			UIManager.setLookAndFeel(new WindowsLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e1) {
+			log.warn(e1.getMessage());
+		}
+		drawWidgets();
+		int width = 600;
+		int height = 400;
+		setSize(width, height);
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				SalesSystemUI.this.domainController.endSession();
+				log.info("SalesSystem closed");
+				System.exit(0);
+			}
+		});
+	}
 
-  private static final Logger log = Logger.getLogger(SalesSystemUI.class);
-
-  // Warehouse model
-  private SalesSystemModel model;
-
-  // Instances of tab classes
-  private PurchaseTab purchaseTab;
-  private HistoryTab historyTab;
-  private StockTab stockTab;
-  private ClientTab clientTab;
-  private SalesDomainController domainController;
-
-  /**
-   * Constructs sales system GUI.
-   * @param domainController Sales domain controller.
-   */
-  public SalesSystemUI(SalesDomainController domainController) {
-      this.domainController = domainController;
-    this.model = new SalesSystemModel(domainController);
-    domainController.setModel(model);
-
-    // Create singleton instances of the tab classes
-    historyTab = new HistoryTab(model);
-    stockTab = new StockTab(model, domainController);
-    purchaseTab = new PurchaseTab(domainController, model, this);
-    clientTab = new ClientTab(model);
-
-    setTitle("Sales system");
-
-    // set L&F to the nice Windows style
-    try {
-        UIManager.setLookAndFeel(new WindowsLookAndFeel());
-    } catch (UnsupportedLookAndFeelException e1) {
-        log.warn(e1.getMessage());
-    }
-
-    drawWidgets();
-
-    // size & location
-    int width = 600;
-    int height = 400;
-    setSize(width, height);
-    Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-    setLocation((screen.width - width) / 2, (screen.height - height) / 2);
-
-    addWindowListener(new WindowAdapter() {
-        @Override
-        public void windowClosing(WindowEvent e) {
-            SalesSystemUI.this.domainController.endSession();
-            log.info("SalesSystem closed");
-            System.exit(0);
-        }
-    });
-  }
-
-  private void drawWidgets() {
-    JTabbedPane tabbedPane = new JTabbedPane();
-
-    tabbedPane.add("Point-of-sale", purchaseTab.draw());
-    tabbedPane.add("Warehouse", stockTab.draw());
-    tabbedPane.add("History", historyTab.draw());
-    tabbedPane.add("Clients", clientTab.draw());
-
-    getContentPane().add(tabbedPane);
-  }
-
+	private void drawWidgets() {
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.add("Point-of-sale", purchaseTab.draw());
+		tabbedPane.add("Warehouse", stockTab.draw());
+		tabbedPane.add("History", historyTab.draw());
+		tabbedPane.add("Clients", clientTab.draw());
+		getContentPane().add(tabbedPane);
+	}
 }
-
-
